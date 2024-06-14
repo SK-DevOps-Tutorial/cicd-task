bfa6e4ccb9f26b704dd5a685d1ec51e079805bc1
Bisi Oloruntola <bisioloruntola@gmail.com>
2024-06-14 20:40:44 +0100

    git file restored

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,10 +2,10 @@ variable "region" {
 default = "us-west-2"
 }
 
-variable "image_id" {
-type =     string
-default  = "ami-0b53285ea6c7a08a7"
-}
+# variable "image_id" {
+# type =     string
+# default  = "ami-05e77069ed898709c"
+# }
 
 variable "instance_type" {
 type =     string
