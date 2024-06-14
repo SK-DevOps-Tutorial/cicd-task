6b8bd89da006879355b5e5a82fcbc35ac6b492c1
Bisi Oloruntola <bisioloruntola@gmail.com>
2024-06-15 00:45:02 +0100

    security group fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,21 +18,37 @@ resource "aws_security_group" "cicd_sg" {
   description = "Allow TLS inbound traffic and all outbound traffic"  # Description of the security group
   vpc_id      = aws_vpc.cicd_vpc.id  # Associate the security group with the specified VPC
 
-  ingress {
-    from_port       = 80  # Port to allow inbound traffic from
-    to_port         = 80  # Port to allow inbound traffic to
-    protocol        = "tcp"  # Protocol for inbound traffic
-    self            = true  # Allow traffic from the security group itself
-    cidr_blocks     = ["0.0.0.0/0"]  # Allow inbound traffic from any IP address
-  }
+  # ingress {
+  #   from_port       = 80  # Port to allow inbound traffic from
+  #   to_port         = 80  # Port to allow inbound traffic to
+  #   protocol        = "tcp"  # Protocol for inbound traffic
+  #   self            = true  # Allow traffic from the security group itself
+  #   cidr_blocks     = ["0.0.0.0/0"]  # Allow inbound traffic from any IP address
+  # }
 
-  egress {
-    from_port       = 0  # Port to allow outbound traffic from
-    to_port         = 0  # Port to allow outbound traffic to
-    protocol        = "tcp"  # Protocol for outbound traffic
-    self            = true  # Allow outbound traffic to the security group itself
-    cidr_blocks     = ["0.0.0.0/0"]  # Allow outbound traffic to any IP address
-  }
+  # egress {
+  #   from_port       = 0  # Port to allow outbound traffic from
+  #   to_port         = 0  # Port to allow outbound traffic to
+  #   protocol        = "tcp"  # Protocol for outbound traffic
+  #   self            = true  # Allow outbound traffic to the security group itself
+  #   cidr_blocks     = ["0.0.0.0/0"]  # Allow outbound traffic to any IP address
+  # }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.cicd_sg.id
+  cidr_ipv4         = aws_vpc.cicd_vpc.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all" {
+  security_group_id = aws_security_group.cicd_sg.id
+  cidr_ipv4   = "10.0.0.0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
 }
 
 resource "aws_vpc" "cicd_vpc" {
