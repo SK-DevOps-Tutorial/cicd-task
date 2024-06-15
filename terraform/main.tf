395293ccefa30f0db72d7ba17279ffb15349e82a
Bisi Oloruntola <bisioloruntola@gmail.com>
2024-06-15 01:06:33 +0100

    security group fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "cicd_webserver" {
   ami =  "ami-0b53285ea6c7a08a7"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.cicd_sg.id]
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
   subnet_id = aws_subnet.public_cicd.id
 
   user_data = <<-EOF
@@ -13,10 +13,10 @@ resource "aws_instance" "cicd_webserver" {
   EOF
 }
 
-resource "aws_security_group" "cicd_sg" {
-  name        = "demo_sg"  # Security group
-  description = "Allow TLS inbound traffic and all outbound traffic"  # Description of the security group
-  vpc_id      = aws_vpc.ci_vpc.id  # Associate the security group with the specified VPC
+# resource "aws_security_group" "cicd_sg" {
+#   name        = "demo_sg"  # Security group
+#   description = "Allow TLS inbound traffic and all outbound traffic"  # Description of the security group
+#   vpc_id      = aws_vpc.ci_vpc.id  # Associate the security group with the specified VPC
 
   # ingress {
   #   from_port       = 80  # Port to allow inbound traffic from
@@ -33,44 +33,68 @@ resource "aws_security_group" "cicd_sg" {
   #   self            = true  # Allow outbound traffic to the security group itself
   #   cidr_blocks     = ["0.0.0.0/0"]  # Allow outbound traffic to any IP address
   # }
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+#   security_group_id = aws_security_group.cicd_sg.id
+#   cidr_ipv4         = aws_vpc.ci_vpc.cidr_block
+#   from_port         = 443
+#   ip_protocol       = "tcp"
+#   to_port           = 443
+# }
+
+# resource "aws_vpc_security_group_egress_rule" "allow_all" {
+#   security_group_id = aws_security_group.cicd_sg.id
+#   cidr_ipv4   = "10.0.0.0/16"
+#   from_port   = 80
+#   ip_protocol = "tcp"
+#   to_port     = 80
+# }
+
+resource "aws_default_vpc" "myvpc" {
+  tags = {
+    Name = "Default VPC"
+  }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.cicd_sg.id
-  cidr_ipv4         = aws_vpc.ci_vpc.cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_default_vpc.myvpc
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all" {
-  security_group_id = aws_security_group.cicd_sg.id
-  cidr_ipv4   = "10.0.0.0/16"
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
-}
-
-resource "aws_vpc" "ci_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
+# resource "aws_vpc" "ci_vpc" {
+#   cidr_block = "10.0.0.0/16"
+# }
 
 resource "aws_subnet" "public_cicd" {
-  vpc_id     = aws_vpc.ci_vpc.id
+  vpc_id     = aws_default_vpc.myvpc.id
   cidr_block = "10.0.1.0/24"
 }
 
   resource "aws_subnet" "private_cicd" {
-  vpc_id     = aws_vpc.ci_vpc.id
+  vpc_id     = aws_default_vpc.myvpc.id
   cidr_block = "10.0.2.0/24"
 }
 
 resource "aws_internet_gateway" "demo_igw" {
-  vpc_id = aws_vpc.ci_vpc.id
+  vpc_id = aaws_default_vpc.myvpc.id
 }
 
 resource "aws_route_table" "demo_rt" {
-  vpc_id = aws_vpc.ci_vpc.id
+  vpc_id = aws_default_vpc.myvpc.id
 
   route {
     cidr_block = "10.0.3.0/24"
